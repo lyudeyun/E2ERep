cbef885a9df23ddac1e7050be9011d86dd4104be
lyudeyun <lyudeyun@gmail.com>
2026-01-21 18:13:31 +0900

    Pick an available TCP port when MASTER_PORT is not set

--- Bench2DriveZoo/adzoo/uniad/uniad_dist_eval.sh
+++ Bench2DriveZoo/adzoo/uniad/uniad_dist_eval.sh
@@ -10,7 +10,16 @@ GPUS=$3                                              #
 # -------------------------------------------------- #
 GPUS_PER_NODE=$(($GPUS<8?$GPUS:8))
 
-MASTER_PORT=${MASTER_PORT:-12145}
+if [ -z "${MASTER_PORT:-}" ]; then
+    MASTER_PORT=$(python - <<'PY'
+import socket
+s = socket.socket()
+s.bind(('', 0))
+print(s.getsockname()[1])
+s.close()
+PY
+)
+fi
 WORK_DIR=$(echo ${CFG%.*} | sed -e "s/configs/work_dirs/g")/
 # Intermediate files and logs will be saved to UniAD/projects/work_dirs/
 
